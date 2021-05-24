d142ae32ba3c9200d55970a74cf53a1e382ce7ee
gawronCode <gawroncode@gmail.com>
2021-05-24 19:01:06 +0200

    Add user list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularApp';
+export class AppComponent implements OnInit {
+  
+  ngOnInit(): void {
+    setTimeout(() => {
+      
+      this.users = []
+      
+      // this.users = [{
+      //   Name: "Janusz",
+      //   SecondName: "Tracz",
+      //   Age: 21,
+      //   Phone: "123456778",
+      //   Email: "tracz@o2.pl",
+      //   RoleName: "Boss"
+      // },{
+      //   Name: "Jan",
+      //   SecondName: "Kowalski",
+      //   Age: 24,
+      //   Phone: "111111111",
+      //   Email: "kowal@o2.pl",
+      //   RoleName: "Gangster"
+      // }];
+    }, 3000);
+  }
+  
+  title: string = 'TAI_Projekt';
+  
+  
+  users;
+
 }
